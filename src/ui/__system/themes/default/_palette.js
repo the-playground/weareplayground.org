a775c6530cf57e53d306656923f7fcc8684da6fd
Christopher Hahn <chris@creativefuse.org>
2019-10-25 16:06:44 -0400

    Add Base for Design System and Theme

---
+++ src/ui/__system/themes/default/_palette.js
@@ -0,0 +1,19 @@
+/**
+ * Theme » Default
+ * Colors » Core
+ *
+ * The colors used throughout the default theme
+ *
+ * @since 1.0.0
+ */
+export const primary = {
+
+}
+
+export const secondary = {
+
+}
+
+export const tertiary = {
+
+}
---
+++ src/ui/__system/themes/default/index.js
@@ -0,0 +1,12 @@
+import { grayscale, system } from '../global'
+import { primary, secondary, tertiary } from './_palette'
+import { ui } from './ui'
+
+const theme = {
+
+	palette: {},
+	ui: {},
+
+}
+
+export default theme;
