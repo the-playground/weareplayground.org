ae2f505db2b69adb8f0b5009fad0865b0b5645bb
Christopher Hahn <chris@creativefuse.org>
2019-11-11 10:20:52 -0500

    Implement Apollo For SSR

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,32 @@ import React from 'react'
 
 import {Layout} from '../lib/components/Layout'
 import {SEOPageMeta} from '../lib/components/SEO'
+import {withApollo} from '../lib/components/Apollo'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const HELLO_QUERY = gql`
+	query HelloQuery {
+		home_page(lang:"en-us", uid:"home"){
+			body{
+      			__typename
+    		}
+		}
+	}
+`
 
 const HomePage = () => {
+
+	const { data, loading, error } = useQuery( HELLO_QUERY );
+
+	if (loading) return <div />
+
+	console.log(data)
 
 	return (
 
 		<Layout>
-
-			Home Page
-
+			<h1></h1>
 			<div hidden id="snipcart" data-api-key={process.env.GATSBY_SNIPCART_API_KEY}></div>
 
 		</Layout>
@@ -19,4 +36,4 @@ const HomePage = () => {
 
 }
 
-export default HomePage
+export default withApollo( HomePage );
