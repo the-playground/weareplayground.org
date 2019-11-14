0dd1f9e181f5ed94021a98bfce661c0ca568cd61
Christopher Hahn <chris@creativefuse.org>
2019-11-14 15:01:22 -0500

    Put Gatsby Home Page Back in Place

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import { Layout } from '../components/Layout';
+import { getPrismicSlice } from '../__utility__/prismic';
+
+const HomePage = ({ data }) => {
+	const globalMeta = data.site.siteMetadata;
+	const pageMeta = getPrismicSlice(data.prismic.home_page.body, 'basic_seo');
+
+	return (
+		<Layout globalSEO={globalMeta} pageSEO={pageMeta}>
+			Coming soon...
+		</Layout>
+	);
+};
+
+export const query = graphql`
+	query HomePageData {
+		...SiteGlobalSEOFragment
+		...PageSEOFragment
+	}
+`;
+
+export default HomePage;
