a6520e116ed70bee0e2b259aa72de2b7863541bb
Christopher Hahn <chris@creativefuse.org>
2019-11-13 20:02:23 -0500

    Pull ESLINT Rules from Wes Bos & Modify

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,10 @@ import { withApollo } from '../src/__utility__/withApollo';
 
 const HOME_PAGE_QUERY = gql`
 	query HomePageQuery {
-		home_page(lang:"en-us", uid:"home"){
+		home_page(lang: "en-us", uid: "home") {
 			page_name
 			body {
-				...on Home_pageBodyBasic_seo {
+				... on Home_pageBodyBasic_seo {
 					type
 					primary {
 						meta_description
@@ -27,20 +27,16 @@ const HOME_PAGE_QUERY = gql`
 `;
 
 const HomePage = () => {
-  const { data, loading, error } = useQuery(HOME_PAGE_QUERY);
+	const { data, loading, error } = useQuery(HOME_PAGE_QUERY);
 
-  if (loading) return <div />;
+	if (loading) return <div />;
 
-  return (
-
-    <Layout>
-
-      <h1>{data.home_page.page_name}</h1>
-      <div hidden id="snipcart" data-api-key={process.env.GATSBY_SNIPCART_API_KEY} />
-
-    </Layout>
-
-  );
+	return (
+		<Layout>
+			<h1>{data.home_page.page_name}</h1>
+			<div hidden id="snipcart" data-api-key={process.env.GATSBY_SNIPCART_API_KEY} />
+		</Layout>
+	);
 };
 
 export default withApollo(HomePage);
