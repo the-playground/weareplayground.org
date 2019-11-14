2538475d79f1c9cf635e6bc71c8aa1746a7be8ac
Christopher Hahn <chris@creativefuse.org>
2019-11-14 15:02:50 -0500

    Remove Old Pages Location
    Gatsby uses `src/pages`

--- pages/shows/index.js
+++
@@ -1,6 +0,0 @@
-import { withApollo } from '../../src/__utility__/withApollo';
-import { Layout } from '../../src/components/Layout';
-
-const ShowsPage = () => <Layout>Show Page</Layout>;
-
-export default withApollo(ShowsPage);
