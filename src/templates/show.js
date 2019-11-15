5e61759968594ac21e5e09a70805334f99642231
Christopher Hahn <chris@creativefuse.org>
2019-11-15 10:17:34 -0500

    Set Up Prop Types Validation Foundation for Show Template

--- src/templates/show.js
+++ src/templates/show.js
@@ -32,9 +32,32 @@ export const query = graphql`
  * Prop Types
  * ----------
  */
+const performancesShape = PropTypes.arrayOf(PropTypes.shape({}));
+const artistsShape = PropTypes.arrayOf(PropTypes.shape({}));
+const sponsorsShape = PropTypes.arrayOf(PropTypes.shape({}));
+const locationShape = PropTypes.shape({});
+const bodyShape = PropTypes.arrayOf(PropTypes.shape({}));
 
 ShowLanding.propTypes = {
-	data: PropTypes.object.isRequired,
+	data: PropTypes.shape({
+		prismic: PropTypes.shape({
+			show: PropTypes.shape({
+				title: PropTypes.string.isRequired,
+				author: PropTypes.string.isRequired,
+				author_link: PropTypes.shape({
+					url: PropTypes.string.isRequired,
+				}),
+				script_link: PropTypes.shape({
+					url: PropTypes.string.isRequired,
+				}),
+				performances: performancesShape,
+				artists: artistsShape,
+				location: locationShape,
+				sponsors: sponsorsShape,
+				body: bodyShape,
+			}).isRequired,
+		}),
+	}).isRequired,
 };
 
 export default ShowLanding;
