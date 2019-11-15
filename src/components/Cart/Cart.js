5b266c0dc991e2cfff520b646f909d7716525338
Christopher Hahn <chris@creativefuse.org>
2019-11-15 18:48:24 -0500

    Set Up Bare Bones Cart Component w/ Snipcart Wrapper

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,5 @@
-const Cart = () => <div id="snipcart" data-api-key={process.env.SNIPCART_API_KEY} hidden />;
+import React from 'react';
+
+const Cart = () => <div id="snipcart" data-api-key={process.env.GATSBY_SNIPCART_API_KEY} hidden />;
 
 export default Cart;
