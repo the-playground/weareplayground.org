caaac40a0936c8f137a1ba779a7102b7caccca7a
Christopher Hahn <chris@creativefuse.org>
2019-11-16 11:04:37 -0500

    Add Stylesheet Loading to Gatsby SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,3 +3,15 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
+
+const React = require('react');
+
+exports.onRenderBody = ({ setHeadComponents }) => {
+	/**
+	 * Load external css SSR
+	 */
+	setHeadComponents([
+		// Snipcart v3
+		<link rel="stylesheet" id="snipcart-css" src="https://cdn.snipcart.com/themes/v3.0.0/default/snipcart.css" type="text/css" />,
+	]);
+};
