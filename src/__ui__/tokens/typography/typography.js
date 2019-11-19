cd07a7c518c034929f2b5a0aff0b08b940ab7556
Christopher Hahn <chris@creativefuse.org>
2019-11-19 08:56:16 -0500

    Begin Building Full Typography Definitions

---
+++ src/__ui__/tokens/typography/typography.js
@@ -0,0 +1,50 @@
+/**
+ * Design Tokens » Typography » Typography
+ *
+ * Pulls together our fonts and font size tokens into an easy to implement
+ * set of style definitions that drive all the typographic styles across the site.
+ *
+ * @uses fonts to pull in the desired fonts for each definition
+ * @uses fontSizes to implement our type scale
+ * @since 1.0.0
+ */
+
+import { css } from 'styled-components';
+
+import fonts from './fonts';
+import { fontSizes } from './typeScale';
+
+const typography = {
+	// BodyText
+	bodyText: {
+		Large: css`
+			${fonts.bodyCommon};
+			${fontSizes.size2};
+		`,
+		Medium: css`
+			${fonts.bodyCommon};
+			${fontSizes.size1};
+		`,
+		Small: css`
+			${fonts.bodyCommon};
+			${fontSizes.size0};
+		`,
+	},
+
+	// Body » Bold
+	bodyBold: {
+		//
+	},
+
+	// Heading » Standard
+	headingStandard: {
+		//
+	},
+
+	// Heading » Grunge
+	headingGrunge: {
+		//
+	},
+};
+
+export default typography;
