9bc90be3bbffafd41518a0cd0a017fb86257733a
Christopher Hahn <chris@creativefuse.org>
2019-11-20 22:36:22 -0500

    Check for SSR before attempting to get URL

--- src/__utility__/URL.js
+++ src/__utility__/URL.js
@@ -1,7 +1,8 @@
+import { isSSR } from './ssr';
+
 /**
  * Return the full current URL path without query strings.
  *
  * TODO: https://github.com/gatsbyjs/gatsby/issues/309
  */
-export const getCurrentRootURL = () =>
-	typeof window !== 'undefined' ? `${window.location.protocol}//${window.location.hostname}${window.location.pathname}` : '';
+export const getCurrentRootURL = () => (isSSR ? '' : `${window.location.protocol}//${window.location.hostname}${window.location.pathname}`);
