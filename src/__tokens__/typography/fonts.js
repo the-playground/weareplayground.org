e1127f510d33088ddbd3ff876d173bb460affaf5
Christopher Hahn <chris@creativefuse.org>
2019-11-20 22:05:37 -0500

    Add Comments to Font Declarations

--- src/__tokens__/typography/fonts.js
+++ src/__tokens__/typography/fonts.js
@@ -2,6 +2,7 @@
  * Design Tokens » Typography » Fonts
  *
  * Define the fonts to use across our project.
+ * Fallback Font Stacks references from: https://css-tricks.com/snippets/css/system-font-stack/
  *
  * @since 1.0.0
  */
@@ -9,7 +10,7 @@
 import { css } from 'styled-components';
 
 const fonts = {
-	//
+	// Greycliff CF Regular
 	bodyText: css`
 		font-family: 'Greycliff', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
 			sans-serif;
@@ -17,7 +18,7 @@ const fonts = {
 		font-weight: 400;
 	`,
 
-	//
+	// Greycliff CF DemiBold
 	bodyTextBold: css`
 		font-family: 'Greycliff', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
 			sans-serif;
@@ -25,7 +26,7 @@ const fonts = {
 		font-weight: 600;
 	`,
 
-	//
+	// Greycliff CF Heavy
 	headingStandard: css`
 		font-family: 'Greycliff', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu,
 			Cantarell sans-serif;
@@ -33,7 +34,7 @@ const fonts = {
 		font-weight: 900;
 	`,
 
-	//
+	// Veneer Grunge
 	headingGrunge: css`
 		font-family: 'Veneer', 'Greycliff', system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell,
 			sans-serif;
