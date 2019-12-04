a8dd5604e93cf968ab03ef96cd147e40f5e38920
Christopher Hahn <chris@creativefuse.org>
2019-12-04 17:38:25 -0500

    Update Link Helper Functions

--- src/__utility__/links.js
+++ src/__utility__/links.js
@@ -5,11 +5,19 @@
  * @param {string} dest
  * @returns {bool}
  */
-export const isLinkInternal = dest => /^\/(?!\/)/.test(dest);
+const isLinkInternal = dest => /^\/(?!\/)/.test(dest);
 
 /**
  * Check to see if the target of the link is a file.
  * @param {*} dest
  * @returns {bool}
  */
-export const isLinkToFile = dest => /^\/(?!\/)/.test(dest);
+const isLinkToFile = dest => /\.[0-9a-z]+$/i.test(dest);
+
+/**
+ * Check to see if the passed path is an external link or a link to a file.
+ *
+ * @param {*} dest
+ * @returns {bool}
+ */
+export const isPageLink = dest => isLinkInternal(dest) || !isLinkToFile(dest);
