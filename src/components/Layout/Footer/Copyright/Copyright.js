3cce3a1cca75bdc8f7bc5cb2b7a0d20609c78fa8
Christopher Hahn <chris@creativefuse.org>
2020-01-24 19:34:35 -0500

    Set Proper Changes from Commit

--- src/components/Layout/Footer/Copyright/Copyright.js
+++ src/components/Layout/Footer/Copyright/Copyright.js
@@ -1,21 +1,10 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import moment from 'moment';
-import { BodyText, BodyBold } from '../../../../_foundations/typography';
-=======
 import { BodyText, BodyBold } from '../../../_foundations/typography';
->>>>>>> Stashed changes
 
 const Copyright = () => {
 	const currentYear = new Date().getFullYear();
 
-<<<<<<< Updated upstream
-	return (
-		<BodyText size="s">Copyright &copy; {currentYear}. The Playground Theatre</BodyText>;
-	)
-=======
 	return <BodyText size="s">Copyright &copy; {currentYear}. The Playground Theatre</BodyText>;
->>>>>>> Stashed changes
 };
 
 export default Copyright;
