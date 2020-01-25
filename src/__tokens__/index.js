156e9c035122798542afc9c545b15bf78f91ea0e
Christopher Hahn <chris@creativefuse.org>
2020-01-25 00:13:34 -0500

    Define Borders & Spacing Tokens

--- src/__tokens__/index.js
+++ src/__tokens__/index.js
@@ -5,4 +5,7 @@ export { default as fonts } from './typography/fonts';
 export { appBaseFontSize } from './typography/typeScale';
 export { default as typography } from './typography/typography';
 
+// Additional configs
+export { borders, borderRadius } from './borders';
 export { default as breakpoints } from './breakpoints';
+export { default as spacing } from './spacing';
