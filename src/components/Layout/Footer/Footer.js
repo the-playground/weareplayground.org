fae0ccbebf0422aa776a0d0a10626b72af55d41b
Christopher Hahn <chris@creativefuse.org>
2020-01-24 19:30:06 -0500

    Init Copyright Build

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
+<<<<<<< Updated upstream
 import { Copyright } from './Copyright';
 
 const Footer = () => (
 	<footer>
 		<Copyright />
 	</footer>
+=======
+import * as Styled from './Footer.styles';
+import { Copyright } from './Copyright';
+
+const Footer = () => (
+	<Styled.Footer>
+		<Copyright />
+	</Styled.Footer>
+>>>>>>> Stashed changes
 );
 
 export default Footer;
