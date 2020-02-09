21bbe0f7a21651c42c2e8ee595e773b989d82ba1
Christopher Hahn <chris@creativefuse.org>
2020-02-09 09:16:38 -0500

    Add Props Validation for Schemas

--- src/components/SEO/StructuredDataGraph/StructuredDataGraph.js
+++ src/components/SEO/StructuredDataGraph/StructuredDataGraph.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
 
 // See Yoast example for how to structure structure data in a graph format.
 
 // `schemas` should be an array of objects
 
-const SEOStructuredDataGraph = schemas => {
-	// Return nothing if the schemas object is empty
-	if (Object.keys(schemas).length === 0) {
-		return <>''</>;
-	}
-
+const SEOStructuredDataGraph = ({ schemas }) => {
 	const dataGraph = `{
 		"@context": "https://schema.org/",
 		"@graph": ${schemas}
 	}`;
 
-	return (
-		<Helmet>
-			<script type="application/ld+json">{dataGraph}</script>
-		</Helmet>
-	);
+	return <Helmet>{schemas ? <script type="application/ld+json">{dataGraph}</script> : ''}</Helmet>;
+};
+
+SEOStructuredDataGraph.propTypes = {
+	schemas: PropTypes.array.isRequired,
 };
 
 export default SEOStructuredDataGraph;
