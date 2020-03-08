f3750b78698f5dfaed20ed6007a491f777bb150e
Christopher Hahn <chris@creativefuse.org>
2020-03-07 22:15:56 -0500

    Prep For Ticket Receipt Email

---
+++ api/__email-templates__/ticketReceipt.js
@@ -0,0 +1,1 @@
+// https://dev.to/powerc9000/sending-emails-with-templates-using-mjml-318e
--- api/actions/ticket-purchased.js
+++ api/actions/ticket-purchased.js
@@ -1,11 +1,16 @@
 import { sendEmail } from '../__utils__/_email';
+// import email template required for this route here.
+// We only want the one we want to be bundled with this serverless funtion
+// Then pass the template into the "sendEmail" function;
 
 /**
  * Orchestrate ticket purchase actions
  *
- * -- send transactional email
- * -- register with Mailchimp or something similar
- * -- set up timed email deliver for day before performance
+ * -- send transactional email (with template)
+ * -- register user with Mailchimp or something similar
+ * -- add user to spreadsheet??
+ * -- set up timed email delivery for the day before performance?
+ * -- submit eCommerce data to Google Analytics & Mailchimp
  */
 
 export default async (req, res) => {
