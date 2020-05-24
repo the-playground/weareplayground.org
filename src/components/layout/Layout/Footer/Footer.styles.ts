462e90c09b2388c1ee866495fe79ec259833a94c
Chris Hahn <chris+digital@chrishahn.me>
2020-05-24 13:33:00 -0400

    Remove Invalid Theme Prop

--- src/components/layout/Layout/Footer/Footer.styles.ts
+++ src/components/layout/Layout/Footer/Footer.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Footer = styled.footer`
-    background-color: ${props => props.theme.ui.backgrounds.dark};
+    background-color: var(--blueD1);
 `;
 /* color: ${props => commonTextColors[props.color]}; */
