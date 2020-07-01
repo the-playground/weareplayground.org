b0c047ed5b425c28c736c60dfb8dbf765a0a3d8a
Chris Hahn <chris+digital@chrishahn.me>
2020-06-30 22:51:26 -0400

    Improve Comments for show status

--- src/__hooks__/shows/useShowStatus.ts
+++ src/__hooks__/shows/useShowStatus.ts
@@ -6,33 +6,45 @@ import isWithinInterval from 'date-fns/isWithinInterval';
 
 import { Performance } from '@type/show';
 
+/**
+ * Using all of the available performances of a show, determine the current status.
+ *
+ * @param performances All of the available performances of the show.
+ */
 export const useShowStatus = (performances: Performance[]) => {
     const [status, setStatus] = useState('inactive');
 
+    // If no performances are passed in, bail.
     if (!performances || performances.length === 0) {
         return { status, setStatus };
     }
 
+    // Grab the first performance of the show and format it.
     const firstPerformance = parseISO(performances[0].datetime);
 
+    // Grab the last performance of the show and format it.
     const lastPerformance = parseISO(
         performances[performances.length - 1].datetime
     );
 
+    // How long before a show opens should we set the status as "upcoming"?
     const upcomingWindowStart = sub(firstPerformance, { days: 30 });
 
+    // The full date range of a production
     const productionInterval = {
         start: firstPerformance,
         end: lastPerformance,
     };
 
+    // The window of time where a show should be considered "coming soon"
     const comingSoonInterval = {
         start: upcomingWindowStart,
-        end: lastPerformance,
+        end: sub(firstPerformance, { hours: 1 }),
     };
 
     const now = new Date();
 
+    // Resolve when one of the following is true
     switch (true) {
         case isPast(lastPerformance):
             setStatus('archived');
