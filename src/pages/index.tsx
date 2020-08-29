8e48f3dc1d84761798aacec64b9dc5e2716f43a3
Chris Hahn <chris+digital@chrishahn.me>
2020-08-29 19:19:58 -0400

    Set Home Page Foundation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,83 @@
 import React from 'react';
 import { graphql, PageProps } from 'gatsby';
 import { GatsbyPageContext } from '@type/gatsby';
+import { PrismicImage } from '@type/prismic';
 
-// Import Typescript interfaces
-import { Layout } from '@components/layout';
+import PageTemplate from '@templates/PageTemplate';
+import { FluidImageProps } from '@components/foundations';
 
 const HomePage: React.FC<PageProps<PageData, GatsbyPageContext>> = ({
     data,
     pageContext,
     location,
 }) => {
+    const { uid } = pageContext;
+    const pageData = data.prismicHomePage;
+
     return (
-        <Layout noHeader={false} noFooter={false}>
+        <PageTemplate
+            slug={uid}
+            pageConfig={pageData}
+            currentLocation={location.pathname}
+        >
             Home Page
-        </Layout>
+        </PageTemplate>
     );
 };
 
-/**
- * ----------
- * Prop Types
- * ----------
- */
+export const query = graphql`
+    query {
+        prismicHomePage {
+            last_publication_date
+            first_publication_date
+            data {
+                # Config & SEO
+                remove_footer
+                remove_header
+                seo_description
+                seo_hide
+                seo_title
+                seo_image {
+                    alt
+                    url
+                    dimensions {
+                        height
+                        width
+                    }
+                }
+
+                # Hero
+                hero_title
+                hero_copy
+                hero_image {
+                    fluid {
+                        ...GatsbyPrismicImageFluid
+                    }
+                }
+
+                # Intro
+            }
+        }
+    }
+`;
 
 interface PageData {
-    prismic: {
-        home_page: {
-            body: any;
+    prismicHomePage: {
+        first_publication_date: string;
+        last_publication_date: string;
+        data: {
+            // Config & SEO
+            remove_header: boolean;
+            remove_footer: boolean;
+            seo_title: string;
+            seo_description: string;
+            seo_image?: PrismicImage;
+            seo_hide: boolean;
+
+            // Hero
+            hero_title: string;
+            hero_copy: string;
+            hero_image: FluidImageProps;
         };
     };
 }
