8f227cbd3252a393423fcf07ed15ab6c8a88919c
Chris Hahn <chris+digital@chrishahn.me>
2020-10-18 13:20:34 -0400

    Simplify `GrittyHeading` with only one line height for new font

--- src/components/foundations/GrittyHeading/__styles.ts
+++ src/components/foundations/GrittyHeading/__styles.ts
@@ -1,78 +1,10 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components';
-import { breakpoints, zIndex, AvailableHeadingTypography } from '@tokens';
+import styled from 'styled-components';
+import { zIndex } from '@tokens';
 import whiteGrit from '@assets/grit-white.png';
 import blackGrit from '@assets/grit-black.png';
 import { GrittyHeadingProps } from './__types';
 
-// A style map of desktop & mobile style for each size the title Highlight may appear as
-const grittyHeadingStyle = {
-    xs: {
-        mobile: {
-            lineHeight: 1.5,
-            paddingBottom: 4,
-        },
-        desktop: {
-            lineHeight: 1.5,
-            paddingBottom: 4,
-        },
-    },
-    s: {
-        mobile: {
-            lineHeight: 1.4,
-            paddingBottom: 5,
-        },
-        desktop: {
-            lineHeight: 1.4,
-            paddingBottom: 6,
-        },
-    },
-    m: {
-        mobile: {
-            lineHeight: 1.5,
-            paddingBottom: 9,
-        },
-        desktop: {
-            lineHeight: 1.5,
-            paddingBottom: 10,
-        },
-    },
-    l: {
-        mobile: {
-            lineHeight: 1.5,
-            paddingBottom: 8,
-        },
-        desktop: {
-            lineHeight: 1.4,
-            paddingBottom: 13,
-        },
-    },
-    xl: {
-        mobile: {
-            lineHeight: 1.4,
-            paddingBottom: 10,
-        },
-        desktop: {
-            lineHeight: 1.4,
-            paddingBottom: 13,
-        },
-    },
-};
-
-// Get the desktop & mobile styles for the specific size
-const getGrittyHeadingStyle = (
-    size: AvailableHeadingTypography
-): FlattenSimpleInterpolation => {
-    const style = grittyHeadingStyle[size];
-
-    return css`
-        line-height: ${style.mobile.lineHeight};
-        padding-bottom: ${style.mobile.paddingBottom}px;
-        ${breakpoints.m} {
-            line-height: ${style.desktop.lineHeight};
-            padding-bottom: ${style.desktop.paddingBottom}px;
-        }
-    `;
-};
+const GRITTY_HEADING_LINE_HEIGHT = 1.3;
 
 export const GrittyHeading = styled.div<
     Pick<GrittyHeadingProps, 'offset' | 'bgColor' | 'size'>
@@ -82,8 +14,7 @@ export const GrittyHeading = styled.div<
     > .text {
         background: ${({ theme, bgColor }) => theme.backgrounds[bgColor].color};
         display: inline;
-        letter-spacing: 5px;
-        ${({ size }) => getGrittyHeadingStyle(size)};
+        line-height: ${GRITTY_HEADING_LINE_HEIGHT};
         position: relative;
         z-index: ${zIndex.base};
     }
