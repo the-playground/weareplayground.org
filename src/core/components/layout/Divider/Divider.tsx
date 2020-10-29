aa9fa27170b85c8b258dc8341cf243588927bb3e
Chris Hahn <chris+digital@chrishahn.me>
2020-10-28 22:05:39 -0400

    Add ClassName to Divider

--- src/core/components/layout/Divider/Divider.tsx
+++ src/core/components/layout/Divider/Divider.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 import { DividerProps } from './Divider.d';
 import * as styled from './Divider.styles';
 
-export const Divider: React.FC<DividerProps> = ({ color }) => (
-    <styled.Divider color={color} />
+export const Divider: React.FC<DividerProps> = ({ color, className }) => (
+    <styled.Divider color={color} className={className} />
 );
