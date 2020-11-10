cabfec5b7693fb0a109eb19ae69832d15e9c678c
Chris Hahn <chris+digital@chrishahn.me>
2020-11-09 22:15:27 -0500

    Environment shouldn’t be undefined

--- packages/web/src/shared/context/EnvironmentContext.tsx
+++ packages/web/src/shared/context/EnvironmentContext.tsx
@@ -11,7 +11,7 @@ export const EnvironmentProvider: React.FC = ({ children }) => {
                     appVersion
                     prodURL
                     deployURL
-                    # environment
+                    environment
                     deployContext
                     deployID
                     commitRef
