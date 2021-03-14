5270c0caa034737be3822f261ae4504ee5dcf11b
Chris Hahn <chris+digital@chrishahn.me>
2021-03-13 19:47:23 -0500

    Add Performance Details Schema Object

--- packages/cms/src/schemas/documents/theatre/show/objects/details.ts
+++ packages/cms/src/schemas/documents/theatre/show/objects/details.ts
@@ -48,3 +48,84 @@ export const detailItem = {
         },
     },
 };
+
+export const performanceDetails = {
+    title: 'Performance Details',
+    name: 'performanceDetails',
+    type: 'object',
+    options: {
+        collapsible: true,
+        collapsed: true,
+    },
+    fields: [
+        {
+            name: 'runtimeHours',
+            title: 'Runtime Hours',
+            description:
+                'How many hours does the show run for (whole numbers only)',
+            type: 'number',
+            validation: (Rule: any) => Rule.integer().positive().lessThan(4),
+        },
+        {
+            name: 'runtimeMinutes',
+            title: 'Runtime Minutes',
+            description:
+                'How many minutes does the show run for (whole numbers only)',
+            type: 'number',
+            validation: (Rule: any) => Rule.integer().positive().lessThan(60),
+        },
+        {
+            name: 'intermissionCount',
+            title: 'Number of Intermissions',
+            description: 'How many intermissions does this show have?',
+            type: 'number',
+            validation: (Rule: any) => Rule.integer().positive().lessThan(3),
+        },
+        {
+            name: 'contentAdvisory',
+            description:
+                'Detailed content disclaimers and warnings with relevant info',
+            type: 'contentAdvisory',
+            options: {
+                collapsible: true,
+                collapsed: true,
+            },
+        },
+        {
+            name: 'effectsAdvisory',
+            type: 'effectsAdvisory',
+            description:
+                'Information about any dangerous or jarring effects used in the show',
+            options: {
+                collapsible: true,
+                collapsed: true,
+            },
+        },
+        {
+            name: 'additionalDetails',
+            title: 'Additional Show Details',
+            description: '',
+            type: 'array',
+            options: {
+                collapsible: true,
+                collapsed: true,
+            },
+            of: [{ type: 'detailItem' }],
+        },
+        {
+            name: 'rating',
+            title: 'Rating',
+            description: 'The age rating/appropriateness of this show.',
+            type: 'string',
+            options: {
+                layout: 'radio',
+                direction: 'horizontal',
+                list: [
+                    { title: 'R', value: 'r' },
+                    { title: 'PG-13', value: 'pg13' },
+                    { title: 'PG', value: 'pg' },
+                ],
+            },
+        },
+    ]
+}
